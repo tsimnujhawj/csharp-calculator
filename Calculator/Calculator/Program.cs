3a8100e3c219f869366737f33c79f30be6ae1023
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-10-31 16:09:18 -0500

    initial commit

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CALCULATOR" +
+                "\n----------");
+
+            var numOne = Console.ReadLine();
+            var expression = Console.ReadLine();
+            var numTwo = Console.ReadLine();
+            
+            Console.WriteLine(numOne + expression + numTwo);
+            Console.ReadLine();
+        }
+    }
+
+    //addition class
+    //class Addition
+    //{
+    //    static int Calc(int args)
+    //    {
+    //        var x = args
+    //        var y = args
+    //        return x + y;
+    //    }
+    //}
+
+    //substraction class
+    //class Substraction
+    //{
+    //    static int Calc(int args)
+    //    {
+
+    //    }
+    //}
+
+    //multiplation class
+
+    //divide class
+}
