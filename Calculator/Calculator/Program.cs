415a45e4b3514e97c6f9746ec6935756732daa41
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-11-08 17:11:44 -0600

    console addition calculator is working

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,10 +6,18 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            byte number;
-            number = 5;
-            float totalPrice = 20.99f;
-            Console.WriteLine(number + totalPrice);
+            Console.WriteLine("Please enter the first number to add:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number to add:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} + {1} = " + Add(a, b), a, b);
+            Console.WriteLine("Press a key to exit...");
+            Console.ReadLine();
+        }
+
+        static public int Add(int a, int b)
+        {
+            return a + b;
         }
     }
 }
